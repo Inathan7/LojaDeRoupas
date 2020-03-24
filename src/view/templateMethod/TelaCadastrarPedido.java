70b79dfb1535346e5149e08140a2da8f3a587604
Inathan <Inathan@LAPTOP-ER1MB744>
2020-03-24 15:18:07 -0300

    Correção de alguns erros.

--- src/view/templateMethod/TelaCadastrarPedido.java
+++ src/view/templateMethod/TelaCadastrarPedido.java
@@ -24,8 +24,8 @@ public class TelaCadastrarPedido extends TemplateMethodTela{
 		setSize(600,350);
 		repaint();
 	}
-	
-	
+
+
 	private JTextField campoEmail;
 	private JTextField campoNome;
 	private JTextField campoTelefone;
@@ -33,7 +33,7 @@ public class TelaCadastrarPedido extends TemplateMethodTela{
 
 	@Override
 	public void adicionarBotoes() {
-		
+
 		JButton buttonVoltar = new JButton("Voltar");
 		buttonVoltar.setBounds(470, 20, 90, 30);
 		add(buttonVoltar);
@@ -41,23 +41,23 @@ public class TelaCadastrarPedido extends TemplateMethodTela{
 		buttonFazerPedido = new JButton("Fazer Pedido");
 		buttonFazerPedido.setBounds(230, 240, 140, 30);
 		add(buttonFazerPedido);
-		
-		OuvinteCadastrarPedido ouvinteCadastrarPedido = new OuvinteCadastrarPedido();
+
+		OuvinteCadastrarPedido ouvinteCadastrarPedido = new OuvinteCadastrarPedido(this);
 		buttonFazerPedido.addActionListener(ouvinteCadastrarPedido);
-		
+
 		OuvinteVoltarTelaCarrinho ouvinteVoltarTelaCarrinho = new OuvinteVoltarTelaCarrinho();
 		buttonVoltar.addActionListener(ouvinteVoltarTelaCarrinho);
-		
+
 	}
 
 	@Override
 	public void adicionarJLabel() {
-		
+
 		JLabel labelTitulo = new JLabel("Dados Do Cliente");
 		labelTitulo.setBounds(225, 80, 150, 30);
 		labelTitulo.setFont(new Font("Times new Roman", Font.BOLD, 20));
 		add(labelTitulo);
-		
+
 		JLabel labelEmail = new JLabel("Email");
 		labelEmail.setBounds(100, 110, 50, 30);
 		labelEmail.setFont(new Font("Arial",Font.BOLD,12));
@@ -67,17 +67,17 @@ public class TelaCadastrarPedido extends TemplateMethodTela{
 		labelNome.setBounds(100, 150, 50, 30);
 		labelNome.setFont(new Font("Arial",Font.BOLD,12));
 		add(labelNome);
-		
+
 		JLabel labelTelefone = new JLabel("Telefone");
 		labelTelefone.setBounds(290,150, 70, 30);
 		labelTelefone.setFont(new Font("Arial",Font.BOLD,12));
 		add(labelTelefone);
-		
+
 	}
-	
-	
+
+
 	public void adicionarTextField() {
-		
+
 		campoEmail = new JTextField();
 		campoEmail.setBounds(170, 110, 290, 30);
 		add(campoEmail);
@@ -99,55 +99,56 @@ public class TelaCadastrarPedido extends TemplateMethodTela{
 			dispose();
 		}
 	}
-	
+
 	public class OuvinteCadastrarPedido implements ActionListener{
 
+		private TelaCadastrarPedido tela;
+
+		public OuvinteCadastrarPedido(TelaCadastrarPedido tela) {
+			this.tela = tela;
+		}
+
 		public void actionPerformed(ActionEvent e) {
-			
-			String opc = e.getActionCommand();
-			
-			switch (opc) {
-			
-			case "Fazer Pedido":
+
+			if(campoEmail.getText().equals("") || campoNome.getText().equals("") || campoTelefone.getText().equals("")) {
+				JOptionPane.showMessageDialog(tela, "Campo Vazio!");
+			}else {
+
 				ControllerPedido pedidoCTL = new ControllerPedido();
 				//Pega as roupas escolhidas pelo o cliente, que está no singleton
 				//que é o carrinho
 				Carrinho carrinho = Carrinho.getInstance();
 				SimpleDateFormat s = new SimpleDateFormat("dd/MM/yyyy");
-			    Date d = new Date();
-			    String a = s.format(d);
-			    String[] datas = a.split("/");
-                datas[2]+=pedidoCTL.quantidadeDePedidos()+1;
-                long id = Long.parseLong(datas[2]);
-                //gera um id com a data do ano junto o tamanho de pedido, assim
-                //o id é unica a cada pedido
+				Date d = new Date();
+				String a = s.format(d);
+				String[] datas = a.split("/");
+				datas[2]+=pedidoCTL.quantidadeDePedidos()+1;
+				long id = Long.parseLong(datas[2]);
+				//gera um id com a data do ano junto o tamanho de pedido, assim
+				//o id é unica a cada pedido
 				pedidoCTL.addPedido(id,campoEmail.getText(), campoNome.getText(),
 						campoTelefone.getText(),carrinho.getRoupa());
 				campoEmail.setText("");
 				campoNome.setText("");
 				campoTelefone.setText("");
-				JOptionPane.showMessageDialog(null, "Pedido Feito!");
+				JOptionPane.showMessageDialog(tela, "Pedido Feito!");
 				//deleta as roupas salvas no singleton carrinho
 				//e depois volta para tela carrinho
 				carrinho.deleteRoupas();
 				new TelaDeCarrinho();
 				dispose();
-				break;
 
-			default:
 
-				
-			// Ainda vou fazer essa parte da lógica, onde vou cadastrar o cliente e fazer a persistência dele.
-			
-			//Paulo - na hora que cria o pedido já é cadastrado o cliente, quando é passado os parametros do pedido,
-			//Como é visto na classe pedido, assim a persistencia de pedido já salva o pedido com o cliente dentro como atributo
-			//a lógica de cadastrar pedido está pegando e funcionando.
-				
-				break;
+				// Ainda vou fazer essa parte da lógica, onde vou cadastrar o cliente e fazer a persistência dele.
+
+				//Paulo - na hora que cria o pedido já é cadastrado o cliente, quando é passado os parametros do pedido,
+				//Como é visto na classe pedido, assim a persistencia de pedido já salva o pedido com o cliente dentro como atributo
+				//a lógica de cadastrar pedido está pegando e funcionando.
+
 			}
-			
-			
+
+
 		}
 	}
-	
+
 }
--- src/view/templateMethod/TelaCadastrarRoupa.java
+++ src/view/templateMethod/TelaCadastrarRoupa.java
@@ -1,12 +1,12 @@
 package view.templateMethod;
 
-import java.awt.Color;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
 import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JTextField;
@@ -16,19 +16,20 @@ import model.Roupa;
 import view.ouvintes.OuvinteVoltarTelaMenu;
 
 public class TelaCadastrarRoupa extends TemplateMethodTela{
+	private TelaCadastrarRoupa tela;
 	/*
 	 * Mudei o ouvinte cadastrar roupa que está nessa classe, eu explico tudo
 	 * no ouvinte que está abaixo.
 	 */
-	
-	
+
+
 	public TelaCadastrarRoupa() {
 		setTitle("Cadastrar Roupas");
 		setSize(600,350);
 		repaint();
 	}
-	
-	
+
+
 	private JTextField campoRoupa;
 	private JTextField campoPreco;
 	private JTextField campoTamanho;
@@ -36,7 +37,7 @@ public class TelaCadastrarRoupa extends TemplateMethodTela{
 
 	@Override
 	public void adicionarBotoes() {
-		
+
 		JButton buttonVoltar = new JButton("Voltar");
 		buttonVoltar.setBounds(370, 240, 90, 30);
 		add(buttonVoltar);
@@ -44,24 +45,24 @@ public class TelaCadastrarRoupa extends TemplateMethodTela{
 		buttonCadastrar = new JButton("Cadastrar");
 		buttonCadastrar.setBounds(100, 240, 90, 30);
 		add(buttonCadastrar);
-		
-		OuvinteCadastrarRoupa ouvinteCadastrarRoupa = new OuvinteCadastrarRoupa();
+
+		OuvinteCadastrarRoupa ouvinteCadastrarRoupa = new OuvinteCadastrarRoupa(this);
 		buttonCadastrar.addActionListener(ouvinteCadastrarRoupa);
 
-	// Adicionar Ouvinte
-	//adicionei o ouvinte voltar para a tela de menu principal.
+		// Adicionar Ouvinte
+		//adicionei o ouvinte voltar para a tela de menu principal.
 		OuvinteVoltarTelaMenu ouvinteTelaMenu = new OuvinteVoltarTelaMenu(this);
 		buttonVoltar.addActionListener(ouvinteTelaMenu);
 	}
 
 	@Override
 	public void adicionarJLabel() {
-		
+
 		JLabel labelTitulo = new JLabel("Cadastrar Roupas");
 		labelTitulo.setBounds(225, 80, 160, 30);
 		labelTitulo.setFont(new Font("Times new Roman", Font.BOLD, 20));
 		add(labelTitulo);
-		
+
 		JLabel labelRoupa = new JLabel("Roupa");
 		labelRoupa.setBounds(100, 110, 50, 30);
 		labelRoupa.setFont(new Font("Arial",Font.BOLD,12));
@@ -71,17 +72,17 @@ public class TelaCadastrarRoupa extends TemplateMethodTela{
 		labelPreco.setBounds(100, 150, 50, 30);
 		labelPreco.setFont(new Font("Arial",Font.BOLD,12));
 		add(labelPreco);
-		
+
 		JLabel labelTamanho = new JLabel("Tamanho");
 		labelTamanho.setBounds(290,150, 70, 30);
 		labelTamanho.setFont(new Font("Arial",Font.BOLD,12));
 		add(labelTamanho);
-		
+
 	}
-	
-	
+
+
 	public void adicionarTextField() {
-		
+
 		campoRoupa = new JTextField();
 		campoRoupa.setBounds(170, 110, 290, 30);
 		add(campoRoupa);
@@ -93,16 +94,22 @@ public class TelaCadastrarRoupa extends TemplateMethodTela{
 		campoTamanho = new JTextField();
 		campoTamanho.setBounds(365, 150, 95, 30);
 		add(campoTamanho);
-		
-		
+
+
 
 	}
-	
-	
+
+
 
 
 
 	public class OuvinteCadastrarRoupa implements ActionListener{
+		
+		private TelaCadastrarRoupa tela;
+		
+		public OuvinteCadastrarRoupa(TelaCadastrarRoupa tela) {
+			this.tela = tela;
+		}
 
 		public void actionPerformed(ActionEvent e) {
 			/*
@@ -114,15 +121,23 @@ public class TelaCadastrarRoupa extends TemplateMethodTela{
 			 * colocar o proximo indice do array para cada roupa que for cadastrada
 			 * como é feito em lista sequencial.
 			 */
-			ControllerRoupa roupaCTL = new ControllerRoupa();
-			ArrayList<Roupa> roupas = roupaCTL.loadRoupa();
-			roupaCTL.addRoupa(campoRoupa.getText(),
-							  Float.parseFloat(campoPreco.getText()),
-							  campoTamanho.getText(),roupas.size()+1);
-			campoRoupa.setText("");
-			campoPreco.setText("");
-			campoTamanho.setText("");
-			JOptionPane.showMessageDialog(null, "Roupa Cadastrada");
+			
+			if(campoRoupa.getText().equals("") || campoPreco.getText().equals("") || campoTamanho.getText().equals("")) {
+				JOptionPane.showMessageDialog(tela, "Campo Vazio!");
+			}else {
+
+				ControllerRoupa roupaCTL = new ControllerRoupa();
+				ArrayList<Roupa> roupas = roupaCTL.loadRoupa(); // Acredito que essa linha acopla camada de negocio com interface.
+				roupaCTL.addRoupa(campoRoupa.getText(),
+						Float.parseFloat(campoPreco.getText()),
+						campoTamanho.getText(),roupas.size()+1);
+				campoRoupa.setText("");
+				campoPreco.setText("");
+				campoTamanho.setText("");
+				JOptionPane.showMessageDialog(tela, "Roupa Cadastrada");
+
+			}
+
 		}
 	}
 }
